453399cd92e55c2ae3ea284b1ea4282a3aa8e7c3
Aleksandr <116497492+impos21sible@users.noreply.github.com>
2024-12-19 03:04:36 +0300

    Add files via upload

---
+++ бд заполнение/lesson_attendance_202412190304.sql
@@ -0,0 +1,11 @@
+INSERT INTO autoschool.lesson_attendance (student_id,lesson_id,attendance_mark) VALUES
+	 ('S1',1,1),
+	 ('S2',2,1),
+	 ('S3',3,0),
+	 ('S4',4,1),
+	 ('S5',5,1),
+	 ('S6',6,0),
+	 ('S7',7,1),
+	 ('S8',8,1),
+	 ('S9',9,0),
+	 ('S10',10,1);
---
+++ бд заполнение/users_202412190304.sql
@@ -0,0 +1,13 @@
+INSERT INTO autoschool.users (first_name,second_name,middle_name,username,"password",role_id) VALUES
+	 ('Анна','Петрова','Вячеславовна','admin1','adminpass',1),
+	 ('Игорь','Смирнов','Олегович','instructor1','instrpass',2),
+	 ('Екатерина','Кузнецова','Андреевна','manager1','managerpass',3),
+	 ('Александр','Иванов','Анатольевич','student1','studentpass',4),
+	 ('Мария','Тихонова','Юрьевна','admin2','adminpass2',5),
+	 ('Андрей','Захаров','Петрович','instructor2','instrpass2',6),
+	 ('Наталья','Соловьева','Игоревна','manager2','managerpass2',7),
+	 ('Михаил','Федоров','Владимирович','student2','studentpass2',8),
+	 ('Юлия','Шевченко','Максимовна','admin3','adminpass3',9),
+	 ('Анатолий','Григорьев','Владиславович','instructor3','instrpass3',10);
+INSERT INTO autoschool.users (first_name,second_name,middle_name,username,"password",role_id) VALUES
+	 ('Анна','Петрова','Вячеславовна','1','1',1);
